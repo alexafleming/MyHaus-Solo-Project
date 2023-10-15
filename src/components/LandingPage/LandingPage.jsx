ba2a55fb22ec2f24628a0553a8b323fcca0ccb14
alexafleming <alexaflemingmn@gmail.com>
2023-10-15 18:54:10 -0500

    added styling to landing page using bootstrap and css

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -12,29 +12,49 @@ function LandingPage() {
   };
 
   return (
-    <div className='container'>
-      <div className='row landing-page-banner'>
+    <div className='container landing-page-container'>
+      <div className='row landing-page-banner mx-auto'>
         <div className='col-4'>
           <img src="images/big-logo.png" alt="Logo" />
         </div>
         <div className='col-8'>
-          <p className="h6 text-center landing-page-desc">
+          <p className="h6 text-center landing-page-desc mx-auto">
             the all-in-one home management app designed to assist you in organizing, documenting, and improving your home projects and maintenance tasks.
           </p>
         </div>
       </div>
       <div className="row landing-page-images">
         <div className="col">
-          <img src="images/decor-image.jpg" alt="Decor" />
+          <div className="rounded-image-container">
+            <img className="rounded-image" src="images/decor-image.jpg" alt="Decor" />
+            <div className="image-overlay">
+              <p className="overlay-text">decor</p>
+            </div>
+          </div>
         </div>
         <div className="col">
-          <img src="images/documents-image.jpg" alt="Documents" />
+          <div className="rounded-image-container">
+            <img className="rounded-image" src="images/documents-image.jpg" alt="Documents" />
+            <div className="image-overlay">
+              <p className="overlay-text">documents</p>
+            </div>
+          </div>
         </div>
         <div className="col">
-          <img src="images/home-projects-image.jpg" alt="Home Projects" />
+          <div className="rounded-image-container">
+            <img className="rounded-image" src="images/home-projects-image.jpg" alt="Home Projects" />
+            <div className="image-overlay">
+              <p className="overlay-text">home projects</p>
+            </div>
+          </div>
         </div>
         <div className="col">
-          <img src="images/appliances-image.jpg" alt="Appliances" />
+          <div className="rounded-image-container">
+            <img className="rounded-image" src="images/appliances-image.jpg" alt="Appliances" />
+            <div className="image-overlay">
+              <p className="overlay-text">appliances</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
