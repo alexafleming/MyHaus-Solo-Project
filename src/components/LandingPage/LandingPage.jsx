09a522a349dabd1ff5a89a8e7a59520e4def132b
alexafleming <alexaflemingmn@gmail.com>
2023-10-15 18:26:30 -0500

    switched styling for landing page to bootstrap over Material UI

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
 
-// CUSTOM COMPONENTS
-import { Container, Typography, Grid } from '@mui/material';
 
 function LandingPage() {
   const [heading, setHeading] = useState('Welcome');
@@ -14,18 +12,32 @@ function LandingPage() {
   };
 
   return (
-    <Container>
-        <Grid container maxWidth="sm" className="landing-page-banner">
-          <Grid item xs={4}>
-          <img src="images/big-logo.png"/>
-          </Grid>
-          <Grid item xs={8}>
-            <Typography variant="h6" align="center" className="landing-page-desc">
-              the all-in-one home management app designed to assist you in organizing, documenting, and improving your home projects and maintenance tasks.
-            </Typography>
-          </Grid>
-        </Grid>
-        </Container>
+    <div className='container'>
+      <div className='row landing-page-banner'>
+        <div className='col-4'>
+          <img src="images/big-logo.png" alt="Logo" />
+        </div>
+        <div className='col-8'>
+          <p className="h6 text-center landing-page-desc">
+            the all-in-one home management app designed to assist you in organizing, documenting, and improving your home projects and maintenance tasks.
+          </p>
+        </div>
+      </div>
+      <div className="row landing-page-images">
+        <div className="col">
+          <img src="images/decor-image.jpg" alt="Decor" />
+        </div>
+        <div className="col">
+          <img src="images/documents-image.jpg" alt="Documents" />
+        </div>
+        <div className="col">
+          <img src="images/home-projects-image.jpg" alt="Home Projects" />
+        </div>
+        <div className="col">
+          <img src="images/appliances-image.jpg" alt="Appliances" />
+        </div>
+      </div>
+    </div>
   );
 }
 
