d4fb877842be54f16c13f02ca032927aa1ef5cd1
alexafleming <alexaflemingmn@gmail.com>
2023-10-16 16:43:44 -0500

    added images from landong page to the about section

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './AboutPage.css';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -7,9 +8,51 @@ import React from 'react';
 
 function AboutPage() {
   return (
-    <div className="container">
-      <div>
-        <p>This about page is for anyone to read!</p>
+    <div className="container about-container">
+      {/* Logo and Description Section */}
+      {/* ... (your existing logo and description section) */}
+      <div className='row'>
+        
+        <div className='col-6'>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="rounded-image-container">
+                <img className="rounded-image decor-image-top" src="images/decor-image.jpg" alt="Decor" />
+                <div className="image-overlay">
+                  <p className="overlay-text">decor</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="rounded-image-container">
+                <img className="rounded-image" src="images/documents-image.jpg" alt="Documents" />
+                <div className="image-overlay">
+                  <p className="overlay-text">documents</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="rounded-image-container">
+                <img className="rounded-image" src="images/home-projects-image.jpg" alt="Home Projects" />
+                <div className="image-overlay">
+                  <p className="overlay-text">home projects</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="rounded-image-container">
+                <img className="rounded-image" src="images/appliances-image.jpg" alt="Appliances" />
+                <div className="image-overlay">
+                  <p className="overlay-text">appliances</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='col-6'></div>
       </div>
     </div>
   );
