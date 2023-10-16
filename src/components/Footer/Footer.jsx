129d1e718e3ddde69f017f5c4a233e421b72f554
alexafleming <alexaflemingmn@gmail.com>
2023-10-16 14:51:38 -0500

    started styling footer comp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -7,7 +8,22 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+  return(
+  <footer class="py-3 my-4 footer-bar">
+    <ul class="nav justify-content-center pb-1 mb-1">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
+      <Link className="nav-link px-2 text-body-secondary" to="/about">
+          About
+        </Link>
+    </ul>
+    <hr className="my-1" />
+    <p className="text-center">&copy; Prime Digital Academy</p>
+   
+  </footer>
+  )
 }
 
 
