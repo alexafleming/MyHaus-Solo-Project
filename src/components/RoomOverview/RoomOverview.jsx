e3b0ba641f6febcba4d0f36da91c28ce0ff8bb28
alexafleming <alexaflemingmn@gmail.com>
2023-10-22 14:22:45 -0500

    fixed accordion to drop down and added a add new section

--- src/components/RoomOverview/RoomOverview.jsx
+++ src/components/RoomOverview/RoomOverview.jsx
@@ -10,11 +10,30 @@ function RoomOverview() {
     return (
         <div>
             <div >
-                <h2>ROOM NAME GOES HERE</h2>
+
+
 
                 <div class="container-overview-page">
                     <div class="row">
                         <div class="col-md-8">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <h2>ROOM NAME GOES HERE</h2>
+                                </div>
+                                <div class="col-md-4 right-align-btn">
+                                    <div class="dropdown">
+                                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            add new +
+                                        </button>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">PAINT COLORS</a></li>
+                                            <li><a class="dropdown-item" href="#">DECOR</a></li>
+                                            <li><a class="dropdown-item" href="#">APPLIANCES + ELECTRONICS</a></li>
+                                            <li><a class="dropdown-item" href="#">MISCELLANEOUS</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                             <div class="accordion accordion-flush" id="accordionFlushExample">
                                 <div class="accordion accordion-flush" id="accordionFlushExample">
                                     <div class="accordion-item">
@@ -24,7 +43,9 @@ function RoomOverview() {
                                             </button>
                                         </h2>
                                         <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                                            <div class="accordion-body"> <code>.accordion-flush</code></div>
+                                            <div class="accordion-body">
+                                                testing
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="accordion-item">
@@ -34,17 +55,19 @@ function RoomOverview() {
                                             </button>
                                         </h2>
                                         <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                                            <div class="accordion-body"><code>.accordion-flush</code></div>
+                                            <div class="accordion-body">
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="accordion-item">
                                         <h2 class="accordion-header">
                                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                                APPLIANCE + ELECTRONICS
+                                                APPLIANCES + ELECTRONICS
                                             </button>
                                         </h2>
                                         <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                                            <div class="accordion-body"><code>.accordion-flush</code></div>
+                                            <div class="accordion-body">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -54,18 +77,17 @@ function RoomOverview() {
                                             MISCELLANEOUS
                                         </button>
                                     </h2>
-                                    <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                                        <div class="accordion-body"><code>.accordion-flush</code></div>
-                                    </div>
+
                                 </div>
                             </div>
+
                         </div>
                         <div class="col-md-3">
-                            <img className="rounded-image" src="images/decor-image.jpg" alt="Decor" />
+                            <img className="image-overview" src="images/decor-image.jpg" />
                             <div class="col-md-4" >
                                 <div className="notes-title">Notes</div>
                                 <div class="form-group">
-                                    <textarea  class="form-control" rows="10" className="notepad"></textarea>
+                                    <textarea class="form-control" rows="10" className="notepad"></textarea>
                                 </div>
                             </div>
                         </div>
@@ -73,6 +95,7 @@ function RoomOverview() {
                 </div>
             </div>
         </div>
+
     );
 }
 
