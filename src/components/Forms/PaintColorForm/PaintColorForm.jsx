3a2813bff54de5029b88c4809f832b1255573dff
alexafleming <alexaflemingmn@gmail.com>
2023-10-22 17:27:20 -0500

    started styling paint  form layout

--- src/components/Forms/PaintColorForm/PaintColorForm.jsx
+++ src/components/Forms/PaintColorForm/PaintColorForm.jsx
@@ -3,13 +3,39 @@ import { useHistory } from 'react-router-dom';
 import './PaintColorForm.css'
 
 function PaintColorForm() {
-    
-  
-  
+
+
+
     return (
-      <div>
-      </div>
+        <div class="container d-flex justify-content-center align-items-center vh-100">
+            <div class="card">
+                <div class="card-body">
+                    <form>
+                        <h2>FORM</h2>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="brandName" placeholder="brand name" />
+                        </div>
+
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="colorName" placeholder="*paint color name" required />
+                        </div>
+
+                        <div class="form-group">
+                            <label for="finish">Paint Finish:</label>
+                            <input type="text" class="form-control" id="finish" placeholder="paint finish" required />
+                        </div>
+
+                        <div class="form-group">
+                            <textarea class="form-control" id="comments" rows="5" placeholder="additional comments"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">SAVE</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
     );
-  }
-  
-  export default PaintColorForm;
+}
+
+export default PaintColorForm;
